33ae53f1326e7d2ab7fa09455bcacecf0f1ffc62
golbkim <dhdiagram@gmail.com>
2019-12-08 22:32:49 +0900

    restful api test

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User,Group
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 from django.shortcuts import render
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='RESTAPI-SWAGGER')
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,5 +17,6 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
 
 
